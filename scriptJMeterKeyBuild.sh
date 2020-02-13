2d133ca47e9e078ab0391c9c91cc7e2562a7e1c5
Matthieu Baumann <matthieu.baumann@leanovia.com>
2020-02-13 15:02:38 +0100

    Can generate key without shared jmeter install

--- scriptJMeterKeyBuild.sh
+++ scriptJMeterKeyBuild.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+echo "creating rmi keystore"
 keytool -genkey -keyalg RSA -alias rmi -keystore rmi_keystore.jks -storepass changeit -keypass changeit -validity 7 -keysize 2048 -dname "CN=a OU=a O=a L=a ST=a C=a"
-mv /rmi_keystore.jks /SharedVolume/apache-jmeter-5.2.1/bin/rmi_keystore.jks
+echo "rmi keystore created"
+echo "moving rmi keystore to shared location"
+mv /rmi_keystore.jks /SharedVolume/rmi_keystore.jks
+
