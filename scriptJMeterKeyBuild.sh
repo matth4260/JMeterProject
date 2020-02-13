921e540b42418b0f82e7e2ce48610572fcf29f24
Matthieu Baumann <matthieu.baumann@leanovia.com>
2020-02-13 10:43:00 +0100

    JMeterKEy Image correctly generating rmi key in SharedVolume/apache/bin

---
+++ scriptJMeterKeyBuild.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+keytool -genkey -keyalg RSA -alias rmi -keystore rmi_keystore.jks -storepass changeit -keypass changeit -validity 7 -keysize 2048 -dname "CN=a OU=a O=a L=a ST=a C=a"
+mv /rmi_keystore.jks /SharedVolume/apache-jmeter-5.2.1/bin/rmi_keystore.jks
