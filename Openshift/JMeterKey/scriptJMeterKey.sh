2fa02b4d95419cdd605c20add50da2dae3db3459
Matthieu Baumann <matthieu.baumann@leanovia.com>
2020-02-18 14:48:59 +0100

    scriptJMeterKey.sh modified, now create key directly in SharedVolume

--- Openshift/JMeterKey/scriptJMeterKey.sh
+++ Openshift/JMeterKey/scriptJMeterKey.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
+/bin/bash
 echo "creating rmi keystore"
-keytool -genkey -keyalg RSA -alias rmi -keystore rmi_keystore.jks -storepass changeit -keypass changeit -validity 7 -keysize 2048 -dname "CN=a OU=a O=a L=a ST=a C=a"
+keytool -genkey -keyalg RSA -alias rmi -keystore /SharedVolume/rmi_keystore.jks -storepass changeit -keypass changeit -validity 7 -keysize 2048 -dname "CN=a OU=a O=a L=a ST=a C=a"
 echo "rmi keystore created"
-echo "moving rmi keystore to shared location"
-mv /rmi_keystore.jks /SharedVolume/rmi_keystore.jks
-
