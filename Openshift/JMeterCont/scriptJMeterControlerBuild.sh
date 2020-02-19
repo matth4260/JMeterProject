af50bbb12a43b2ce4d46b75ba6383b2243d45476
Matthieu Baumann <matthieu.baumann@leanovia.com>
2020-02-19 14:41:58 +0100

    job for controler created

--- Openshift/JMeterCont/scriptJMeterControlerBuild.sh
+++ Openshift/JMeterCont/scriptJMeterControlerBuild.sh
@@ -4,4 +4,4 @@ list_ips=$(echo $(dig jmeter-serv.jmeter 1 +search +short) | sed 's/ /,/g')
 echo "Liste des ips : $list_ips"
 cp /SharedVolume/rmi_keystore.jks /apache-jmeter-5.2.1/bin/rmi_keystore.jks
 cd /apache-jmeter-5.2.1/bin/
-/apache-jmeter-5.2.1/bin/jmeter -n -R$list_ips -t /SharedVolume/JMeterTest.jmx -l /SharedVolume/results.jtl -e -o /SharedVolume/Results
+/apache-jmeter-5.2.1/bin/jmeter -n -R$list_ips -X -t /SharedVolume/JMeterTest.jmx -l /SharedVolume/results.jtl -e -o /SharedVolume/Results
