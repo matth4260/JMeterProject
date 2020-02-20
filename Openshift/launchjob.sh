6637ffa3b7d37ebd848c31c78beefdec85df6c2e
Matthieu Baumann <matthieu.baumann@leanovia.com>
2020-02-20 09:23:59 +0100

    launchjob.sh now launches all 3 jobs

--- Openshift/launchjob.sh
+++ Openshift/launchjob.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-response=$(curl -k -X POST -d @jobAPI.json -H "Authorization: Bearer R6vxuQvub9cjZUgjVOBh359hrOaWEyaamigXiULz_cI" -H 'Accept: application/json' -H 'Content-Type: application/json' https://cloud.leanovia.net:8443/apis/batch/v1/namespaces/jmeter/jobs)
-echo $response
+responsekey=$(curl -k -X POST -d @json/jobjmeterkey.json -H "Authorization: Bearer R6vxuQvub9cjZUgjVOBh359hrOaWEyaamigXiULz_cI" -H 'Accept: application/json' -H 'Content-Type: application/json' https://cloud.leanovia.net:8443/apis/batch/v1/namespaces/jmeter/jobs)
+sleep 10
+responseserv=$(curl -k -X POST -d @json/jobjmeterserv.json -H "Authorization: Bearer R6vxuQvub9cjZUgjVOBh359hrOaWEyaamigXiULz_cI" -H 'Accept: application/json' -H 'Content-Type: application/json' https://cloud.leanovia.net:8443/apis/batch/v1/namespaces/jmeter/jobs)
+sleep 20
+responsecont=$(curl -k -X POST -d @json/jobjmetercont.json -H "Authorization: Bearer R6vxuQvub9cjZUgjVOBh359hrOaWEyaamigXiULz_cI" -H 'Accept: application/json' -H 'Content-Type: application/json' https://cloud.leanovia.net:8443/apis/batch/v1/namespaces/jmeter/jobs)
+
 
 #curl -k -X POST -d @jobAPI.json -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/jmeter/jobs
 #curl -k -X POST -d @jobAPI.json -H "Authorization: Bearer R6vxuQvub9cjZUgjVOBh359hrOaWEyaamigXiULz_cI" -H 'Accept: application/json' -H 'Cntent-Type: application/json' https://cloud.leanovia.net:8443/apis/batch/v1/namespaces/jmeter/jobs
