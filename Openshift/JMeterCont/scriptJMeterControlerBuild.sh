2fa4007e0c4decf258527ef0c472ea4ff4298eb1
Matthieu Baumann <matthieu.baumann@leanovia.com>
2020-04-08 09:59:25 +0200

    modified controler and server scripts, now copy test or csv to bin

--- Openshift/JMeterCont/scriptJMeterControlerBuild.sh
+++ Openshift/JMeterCont/scriptJMeterControlerBuild.sh
@@ -4,4 +4,6 @@ list_ips=$(echo $(dig jmeter-serv.jmeter 1 +search +short) | sed 's/ /,/g')
 echo "Liste des ips : $list_ips"
 cp /SharedVolume/rmi_keystore.jks /apache-jmeter-5.2.1/bin/rmi_keystore.jks
 cd /apache-jmeter-5.2.1/bin/
-/apache-jmeter-5.2.1/bin/jmeter -n -R$list_ips -X -t /SharedVolume/JMeterTest.jmx -l /SharedVolume/results.jtl -e -o /SharedVolume/Results
+cp /SharedVolume/JMeterTest.jmx /apache-jmeter-5.2.1/bin/JMeterTest.jmx
+
+/apache-jmeter-5.2.1/bin/jmeter -n -R$list_ips -X -t ./JMeterTest.jmx -l /SharedVolume/results.jtl -e -o /SharedVolume/Results
--- Openshift/JMeterServ/scriptJMeterServerBuild.sh
+++ Openshift/JMeterServ/scriptJMeterServerBuild.sh
@@ -2,4 +2,7 @@
 
 cp /SharedVolume/rmi_keystore.jks /apache-jmeter-5.2.1/bin/rmi_keystore.jks
 cd /apache-jmeter-5.2.1/bin/
+
+cp /SharedVolume/user_creation_compte.csv /apache-jmeter-5.2.1/bin/user_creation_compte.csv
+
 /apache-jmeter-5.2.1/bin/jmeter-server
