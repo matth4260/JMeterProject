fcd8f150673bb300b74a831ab7e8e3e9409f54e8
Matthieu Baumann <matthieu.baumann@leanovia.com>
2020-04-10 14:54:22 +0200

    now managing errors

--- Openshift/JMeterMain/jmeterDecoupeCSV.py
+++ Openshift/JMeterMain/jmeterDecoupeCSV.py
@@ -6,45 +6,50 @@ def splitcsv(name, list_ip):
 	file_path = '/SharedVolume/csv/'
 	dest_path = '/SharedVolume/csvModif/'
 	file_path = file_path + name + ".csv"
-	print("Filepath = " + file_path)
-	csv_all_lines = open(file_path, 'r',encoding='utf-8').readlines()
 
-	# extraction du header
-	header = csv_all_lines[0]
+	try:
+		csv_all_lines = open(file_path, 'r',encoding='utf-8').readlines()
 
-	# supprimer le header
-	csv_all_lines.pop(0)
+		# extraction du header
+		header = csv_all_lines[0]
 
-	# Nombre de lignes
-	row_count = len(csv_all_lines)
-	print("nombre de lignes : " + str(row_count))
+		# supprimer le header
+		csv_all_lines.pop(0)
 
-	nb_ip = len(list_ip)
-	print("Nombre d'ip : " + str(nb_ip))
+		# Nombre de lignes
+		row_count = len(csv_all_lines)
 
-	record_per_file = row_count // nb_ip
-	print("nb de ligne par nouveau fichier : " + str(record_per_file))
+		nb_ip = len(list_ip)
 
-	for i in range(len(list_ip)):
+		record_per_file = row_count // nb_ip
 
-		write_file = header
-		for j in csv_all_lines[i*record_per_file:(i+1)*record_per_file]:
-			write_file += j
-		# creation du fichier
-		open(dest_path + name+'-'+str(list_ip[i])+'.csv', 'w+',encoding='utf-8').writelines(write_file)
+		for i in range(len(list_ip)):
 
+			write_file = header
+			for j in csv_all_lines[i*record_per_file:(i+1)*record_per_file]:
+				write_file += j
+			# creation du fichier
+			open(dest_path + name+'-'+str(list_ip[i])+'.csv', 'w+',encoding='utf-8').writelines(write_file)
+		return 0
+	except:
+		return 1
+
+	
 
 list_ip = sys.argv[1].split(',')
 DIR = "/SharedVolume/csv/"
+WORKING = "True"
 
 #DIR = "csv/"
 
 for file in os.listdir(DIR):
 	if file.endswith(".csv"):
 		file_name = file[0:-4]
-		splitcsv(file_name,list_ip)
+		if splitcsv(file_name,list_ip) != 0:
+			WORKING = "False"
 
 
 if not os.path.exists('/SharedVolume/csvModif/READY'): 
 	file = open("/SharedVolume/csvModif/READY",'w',encoding='utf-8')
 
+print(WORKING)
--- Openshift/JMeterMain/launchjob.sh
+++ Openshift/JMeterMain/launchjob.sh
@@ -48,64 +48,78 @@ if test -f "$FILE"; then
     response=$(python3 jmeterDecoupeCSV.py $LIST_IP)
     echo "Fin découpage"
 
-
-    ready=False
-    i=1
-    while [ "$ready" == "False" ] && [ $i -lt  $MAXLOOP ]
-    do
-      echo "Cheking if servers are waiting for controler"
-      
-      ipsAvecEspace=${LIST_IP//,/ }
-
-      ready=True
-
-      for ip in $ipsAvecEspace
+    if [ "$response" == "True" ]; then
+      echo "Découpage bien effectué"
+      ready=False
+      i=1
+      while [ "$ready" == "False" ] && [ $i -lt  $MAXLOOP ]
       do
-        curl $ip":1099"
-        reponse=$?
-        if [ $reponse != 0 ];
-        then
-          eady=False
+        echo "Cheking if servers are waiting for controler"
+        
+        ipsAvecEspace=${LIST_IP//,/ }
+
+        ready=True
+
+        for ip in $ipsAvecEspace
+        do
+          curl $ip":1099"
+          reponse=$?
+          if [ $reponse != 0 ];
+          then
+            ready=False
+          fi
+        done
+
+        sleep 10
+        i=$(($i+1))
+      done
+
+      if [ "$ready" == "True" ]; then
+        echo "JMeter servers started properly, launching controler job"
+        responsecont=$(curl -k -X POST -d @json/jobjmetercont.json -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs)
+        finished="False"
+        i=0
+        while [ $finished == "False" ] && [ $i -lt  $MAXLOOP ]
+        do
+          echo "checking if servers are finished"
+          sleep 20
+          finished=$(curl -k     -H "Authorization: Bearer $TOKEN"     -H 'Accept: application/json'     https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmetercont | python3 jmeterServEnded.py )
+          i=$(($i+1))
+        done
+        if [ "$finished" == "True" ]; then
+          echo "Test finished"
+        else
+          echo "Test took too long to finish, now stopping it"
         fi
-      done
-
-      sleep 5
-      i=$(($i+1))
-    done
-
-    if [ "$ready" == "True" ]; then
-      echo "JMeter servers didn't start properly, launching controler job"
-      responsecont=$(curl -k -X POST -d @json/jobjmetercont.json -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs)
-      finished="False"
-      while [ $finished == "False" ]
-      do
-        echo "checking if servers are finished"
-        sleep 5
-        finished=$(curl -k     -H "Authorization: Bearer $TOKEN"     -H 'Accept: application/json'     https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmetercont | python3 jmeterServEnded.py )
-
-      done
-      echo "closing jobs"
-      responsedeletekey=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmeterkey)
-      responsedeleteserv=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmeterserv)
-      responsedeletecont=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmetercont)
-      responsedeletekey=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterkey)
-      responsedeleteserv=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterserv)
-      responsedeletecont=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmetercont)
-      echo "all jobs and pods closed"
+        echo "closing controler job and container"
+        responsedeletecont=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmetercont)
+        responsedeletecont=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmetercont)
+      else
+        echo "jmeter servers didn't start properly"
+      fi
+    
     else
-      echo "jmeter servers didn't start properly"
+      echo "Problème avec le découpage des csv"
     fi
+
 
   else
     echo "The servers didn't start properly"
   fi
-
+  echo "Stopping the server job, and the servers' containers"
+  responsedeleteserv=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmeterserv)
+  responsedeleteserv=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterserv)
 
   
 else
   echo "rmi_keystore couldn't be generated"
   echo "Stopping the test"
 fi
+echo "Stopping key job and container"
+responsedeletekey=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmeterkey)
+responsedeletekey=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterkey)
+echo "all jobs and pods closed"
+
 
 
 
