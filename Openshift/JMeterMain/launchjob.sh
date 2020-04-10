e569a6a552da40705bf61e3cb1904542daed78d5
Matthieu Baumann <matthieu.baumann@leanovia.com>
2020-04-10 13:40:39 +0200

    launchjob now checking more errors

--- Openshift/JMeterMain/launchjob.sh
+++ Openshift/JMeterMain/launchjob.sh
@@ -10,87 +10,103 @@ echo "Launching key job"
 responsekey=$(curl -k -X POST -d @json/jobjmeterkey.json -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs)
 
 ready=False
-while [ "$ready" == False ]
+i=0
+while [ "$ready" == False ] && [ $i -lt  $MAXLOOP ]
 do
-        echo "Checking if key container is done"
-        ready=$(curl -k     -H "Authorization: Bearer $TOKEN"     -H 'Accept: application/json'     https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterkey | python3 jmeterServEnded.py)
-        sleep 10
+  echo "Checking if key container is done"
+  ready=$(curl -k     -H "Authorization: Bearer $TOKEN"     -H 'Accept: application/json'     https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterkey | python3 jmeterServEnded.py)
+  sleep 10
+	i=$(($i+1))
 done
 
 FILE=/SharedVolume/rmi_keystore.jks
 if test -f "$FILE"; then
-    echo "$FILE exist"
-fi
+  echo "rmi_keystore generated"
+  response=$(python3 jmeterServChangeNumberInjector.py)
+  echo response
+  echo "Launching servers job"
+  responseserv=$(curl -k -X POST -d @json/jobjmeterserv.json -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs)
 
-response=$(python3 jmeterServChangeNumberInjector.py)
-echo response
-echo "Launching servers job"
-responseserv=$(curl -k -X POST -d @json/jobjmeterserv.json -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs)
-
-ready=False
-while [ "$ready" == False ]
-do
-	echo "Checking if servers are ready"
-	ready=$(curl -k     -H "Authorization: Bearer $TOKEN"     -H 'Accept: application/json'     https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterserv | python3 jmeterServReady.py)
-	sleep 10
-done
-
-
-#Récupérer la liste des ips
-echo "liste des ips : "
-export LIST_IP=$(echo $(dig jmeter-serv.jmeter 1 +search +short) | sed 's/ /,/g')
-echo $LIST_IP
-
-echo "Découpage des fichiers CSVs"
-response=$(python3 jmeterDecoupeCSV.py $LIST_IP)
-echo "Fin découpage"
-
-
-ready=False
-i=1
-while [ "$ready" == "False" ] && [ $i -lt  $MAXLOOP ]
-do
-	echo "Cheking if servers are waiting for controler"
-	
-	ipsAvecEspace=${LIST_IP//,/ }
-
-	ready=True
-
-	for ip in $ipsAvecEspace
-	do
-        	curl $ip":1099"
-        	reponse=$?
-        	if [ $reponse != 0 ];
-        	then
-                	ready=False
-        	fi
-	done
-
-	sleep 5
-	i=$(($i+1))
-done
-
-if [ "$ready" == "True" ]; then
-  echo "Servers ready launching controler job"
-	responsecont=$(curl -k -X POST -d @json/jobjmetercont.json -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs)
-  finished="False"
-  while [ $finished == "False" ]
+  ready=False
+  i=0
+  while [ "$ready" == False ] && [ $i -lt  $MAXLOOP ]
   do
-    echo "checking if servers are finished"
-    sleep 5
-    finished=$(curl -k     -H "Authorization: Bearer $TOKEN"     -H 'Accept: application/json'     https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmetercont | python3 jmeterServEnded.py )
-
+    echo "Checking if servers are ready"
+    ready=$(curl -k     -H "Authorization: Bearer $TOKEN"     -H 'Accept: application/json'     https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterserv | python3 jmeterServReady.py)
+    sleep 10
   done
-  echo "closing jobs"
-  responsedeletekey=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmeterkey)
-  responsedeleteserv=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmeterserv)
-  responsedeletecont=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmetercont)
-  responsedeletekey=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterkey)
-  responsedeleteserv=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterserv)
-  responsedeletecont=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmetercont)
-  echo "all jobs and pods closed"
+
+  if [ "$ready" == "True" ]; then
+    echo "The servers are ready"
+
+    #Récupérer la liste des ips
+    echo "liste des ips : "
+    export LIST_IP=$(echo $(dig jmeter-serv.jmeter 1 +search +short) | sed 's/ /,/g')
+    echo $LIST_IP
+
+    echo "Découpage des fichiers CSVs"
+    response=$(python3 jmeterDecoupeCSV.py $LIST_IP)
+    echo "Fin découpage"
+
+
+    ready=False
+    i=1
+    while [ "$ready" == "False" ] && [ $i -lt  $MAXLOOP ]
+    do
+      echo "Cheking if servers are waiting for controler"
+      
+      ipsAvecEspace=${LIST_IP//,/ }
+
+      ready=True
+
+      for ip in $ipsAvecEspace
+      do
+        curl $ip":1099"
+        reponse=$?
+        if [ $reponse != 0 ];
+        then
+          eady=False
+        fi
+      done
+
+      sleep 5
+      i=$(($i+1))
+    done
+
+    if [ "$ready" == "True" ]; then
+      echo "JMeter servers didn't start properly, launching controler job"
+      responsecont=$(curl -k -X POST -d @json/jobjmetercont.json -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs)
+      finished="False"
+      while [ $finished == "False" ]
+      do
+        echo "checking if servers are finished"
+        sleep 5
+        finished=$(curl -k     -H "Authorization: Bearer $TOKEN"     -H 'Accept: application/json'     https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmetercont | python3 jmeterServEnded.py )
+
+      done
+      echo "closing jobs"
+      responsedeletekey=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmeterkey)
+      responsedeleteserv=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmeterserv)
+      responsedeletecont=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' -H 'Content-Type: application/json' https://$ENDPOINT/apis/batch/v1/namespaces/$NAMESPACE/jobs/jobjmetercont)
+      responsedeletekey=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterkey)
+      responsedeleteserv=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmeterserv)
+      responsedeletecont=$(curl -k -X DELETE -H "Authorization: Bearer $TOKEN" -H 'Accept: application/json' https://$ENDPOINT/api/v1/namespaces/$NAMESPACE/pods?labelSelector=job-name=jobjmetercont)
+      echo "all jobs and pods closed"
+    else
+      echo "jmeter servers didn't start properly"
+    fi
+
+  else
+    echo "The servers didn't start properly"
+  fi
+
+
+  
 else
-	echo "servers didn't start properly"
+  echo "rmi_keystore couldn't be generated"
+  echo "Stopping the test"
 fi
+
+
 
 
