8b2eabb8849727fab42b8e43687aad01fc1068ce
Matthieu Baumann <matthieu.baumann@leanovia.com>
2020-04-14 15:59:26 +0200

    jmx test now configurable with config map

--- Openshift/JMeterCont/scriptJMeterControlerBuild.sh
+++ Openshift/JMeterCont/scriptJMeterControlerBuild.sh
@@ -4,6 +4,6 @@ list_ips=$(echo $(dig jmeter-serv.jmeter 1 +search +short) | sed 's/ /,/g')
 echo "Liste des ips : $list_ips"
 cp /SharedVolume/rmi_keystore.jks /apache-jmeter-5.2.1/bin/rmi_keystore.jks
 cd /apache-jmeter-5.2.1/bin/
-cp /SharedVolume/JMeterTest.jmx /apache-jmeter-5.2.1/bin/JMeterTest.jmx
+cp /SharedVolume/${JMX_NAME}.jmx /apache-jmeter-5.2.1/bin/${JMX_NAME}.jmx
 
-/apache-jmeter-5.2.1/bin/jmeter -n -R$list_ips -X -t ./JMeterTest.jmx -l /SharedVolume/results.jtl -e -o /SharedVolume/Results
+/apache-jmeter-5.2.1/bin/jmeter -n -R$list_ips -X -t ${JMX_NAME}.jmx -l /SharedVolume/results.jtl -e -o /SharedVolume/Results
